5055acc07865fa66bd2afdac04f37026d7f56695
Florin Matei <office@pixeltime.ro>
2023-09-16 23:34:39 +0300

    update sidebar

--- website/src/app/pages/blog/sidebar-blog/sidebar-blog.component.ts
+++ website/src/app/pages/blog/sidebar-blog/sidebar-blog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-sidebar-blog',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarBlogComponent implements OnInit {
 
-  constructor() { }
+  public lastItems !: Array<any>
+  constructor(private http:HttpClient) { }
 
-  ngOnInit(): void {
+  public getLast5Posts(): Observable<Array<{}>> {
+    const url = 'https://pixeltime.ro/wp/wp-json/wp/v2/posts?per_page=5';
+    return this.http.get<Array<{}>>(url);
+  }
+  ngOnInit() {
+    this.getLast5Posts().subscribe(data => {
+      this.lastItems = data
+    })
   }
 
 }
