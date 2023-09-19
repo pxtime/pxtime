000432cea2cd0f05eb14661ab9433dffe2af7ea3
Florin Matei <office@pixeltime.ro>
2023-09-19 09:53:30 +0300

    blog pagination

--- website/src/app/pages/blog/blog.component.ts
+++ website/src/app/pages/blog/blog.component.ts
@@ -1,28 +1,98 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {PostInterface} from "../../interfaces/post.interface";
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { PostInterface } from "../../interfaces/post.interface";
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss']
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent implements OnInit, OnChanges {
 
-  public currentPage = 1;
-  public itemsPerPage = 4;
-  public totalItemsPerPage!: Array<PostInterface>
-  public totalPages:Array<number> = [];
-  public totalItems!: number
-  public activePage!:number
+  @Input() totalItems!: number;
+  @Input() itemsPerPage: number = 3;
+  currentPage = 1;
+  totalPages!: number;
+  visiblePages: number[] = [];
+  totalItemsPerPage: PostInterface[] = [];
 
   constructor(private http: HttpClient) {
   }
 
-  public getPost(): Observable<Array<PostInterface>> {
+  ngOnChanges() {
+    this.totalPages = this.getTotalPages();
+    this.updateVisiblePages();
+  }
+
+  getTotalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  firstPage() {
+    if (this.currentPage > 1) {
+      this.currentPage = 1;
+      this.updateVisiblePages();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updateVisiblePages();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updateVisiblePages();
+    }
+  }
+
+  lastPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage = this.totalPages;
+      this.updateVisiblePages();
+    }
+  }
+
+  selectPage(page: number) {
+    this.currentPage = page;
+    this.updateVisiblePages();
+  }
+  updateVisiblePages() {
+    const totalPages = this.totalPages;
+    const currentPage = this.currentPage;
+    const maxPagesToShow = 5;
+
+    this.visiblePages = [];
+
+    if (totalPages <= maxPagesToShow) {
+      for (let page = 1; page <= totalPages; page++) {
+        this.visiblePages.push(page);
+      }
+    } else {
+      if (currentPage <= Math.ceil(maxPagesToShow / 2)) {
+        for (let page = 1; page <= maxPagesToShow; page++) {
+          this.visiblePages.push(page);
+        }
+      } else if (currentPage >= totalPages - Math.floor(maxPagesToShow / 2)) {
+        for (let page = totalPages - maxPagesToShow + 1; page <= totalPages; page++) {
+          this.visiblePages.push(page);
+        }
+      } else {
+        for (let page = currentPage - 2; page <= currentPage + 2; page++) {
+          this.visiblePages.push(page);
+        }
+      }
+    }
+  }
+
+
+  public getPost(): Observable<PostInterface[]> {
     const url = `https://pixeltime.ro/wp/wp-json/wp/v2/posts?per_page=${this.itemsPerPage}&page=${this.currentPage}`;
-    return this.http.get<Array<PostInterface>>(url);
+    return this.http.get<PostInterface[]>(url);
   }
 
   public getNumberOfPosts(): Observable<any> {
@@ -30,56 +100,15 @@ export class BlogComponent implements OnInit {
     return this.http.get<any>(url);
   }
 
-  public nextPage() {
-    if(this.currentPage===this.getTotalPages())
-      return;
-    this.currentPage = this.currentPage + 1;
-    this.getSelectedPage(this.currentPage);
-    this.getPost().subscribe(response => {
-      this.totalItemsPerPage = response
-    })
-  }
-
-  public prevPage() {
-    if(this.currentPage===1){
-      return;
-    }
-    this.currentPage = this.currentPage - 1;
-    this.getSelectedPage(this.currentPage);
-    this.getPost().subscribe(response => {
-      this.totalItemsPerPage = response
-    })
-  }
-
-  public getSelectedPage(numberOfPage:number){
-    this.activePage = numberOfPage;
-  }
-  public selectPage(numberOfPage:number){
-    this.currentPage = numberOfPage;
-    this.getPost().subscribe(response => {
-      this.totalItemsPerPage = response
-    })
-    this.getSelectedPage(numberOfPage);
-  }
-
-  getTotalPages(): number {
-    return Math.ceil(this.totalItems / this.itemsPerPage);
-  }
-
   ngOnInit() {
-    this.currentPage=1;
-    this.activePage=1;
+    this.getNumberOfPosts().subscribe(response => {
+      this.totalItems = response[0].count;
+      this.totalPages = this.getTotalPages();
+      this.updateVisiblePages();
+    });
 
     this.getPost().subscribe(response => {
-      this.totalItemsPerPage = response
-
-    })
-    this.getNumberOfPosts().subscribe(response => {
-      this.totalItems = response[0].count
-
-      for (let page = 1; page <= this.getTotalPages(); page++) {
-        this.totalPages.push(page);
-      }
-    })
+      this.totalItemsPerPage = response;
+    });
   }
 }
